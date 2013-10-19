370aa24e011bbf3db720f9eae48cd6f2a9fc8141
Naoto Kaneko <naoty.k@gmail.com>
2013-10-19 19:18:04 +0900

    Install git

---
+++ site-cookbooks/apt-get/metadata.rb
@@ -0,0 +1,7 @@
+name             'apt-get'
+maintainer       'Naoto Kaneko'
+maintainer_email 'naoty.k@gmail.com'
+license          'MIT'
+description      'Installs/Configures apt-get'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ spec/vm-raspberry_pi/git_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+
+describe package("git") do
+  it { should be_installed }
+end
+
